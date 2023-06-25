c9dfaf70f60ca32cb41be9bb2a67260f8ee5bd35
Igor <test@mail.ru>
2023-06-25 23:06:50 +0300

    Добавил услования задач 36 и 38 (task 36 and task 38)

---
+++ task38/Program.cs
@@ -0,0 +1,5 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+// Random rand = new Random();
+// double number = Math.Round(rand.Next(0,10) + rand.NextDouble(), 3);
+// System.Console.WriteLine(number);
