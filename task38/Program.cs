4b5e08b578a9a6dbdad4e2a347571deb4e9154e9
Igor <test@mail.ru>
2023-06-26 10:56:02 +0300

    Добавил решение задачи 38 (task38)

--- task38/Program.cs
+++ task38/Program.cs
@@ -3,8 +3,21 @@
 // Random rand = new Random();
 // double number = Math.Round(rand.Next(0,10) + rand.NextDouble(), 3);
 // System.Console.WriteLine(number);
-int[] arr = { 5, 120, 4, 3, 2, 6, 7, 100, 10 };
-void PrintArray(int[] array)
+// int[] arr = { 5, 120, 4, 3, 2, 6, 7, 100, 10 };
+double[] FillArray(int size, int leftRange, int rightRange)
+{
+    double[] tempArray = new double[size];
+    Random rand = new Random();
+    //double number = Math.Round(rand.Next(0,10) + rand.NextDouble(), 3);
+
+    for (int i = 0; i < tempArray.Length; i++)
+    {
+        //tempArray[i] = rand.Next(leftRange, rightRange + 1);
+        tempArray[i] = Math.Round(rand.Next(0,10) + rand.NextDouble(), 3);
+    }
+    return tempArray;
+}
+void PrintArray(double[] array)
 {
     int count = array.Length;
     for(int i = 0 ; i < count; i++)
@@ -13,9 +26,9 @@ void PrintArray(int[] array)
     }
     Console.WriteLine();
 }
-void F_min(int[] array, out int min)
+void F_min(double[] array, out double min)
 {
-min = arr[0];
+min = array[0];
 {
     for(int i = 1; i < array.Length; i++)
     {
@@ -24,9 +37,9 @@ min = arr[0];
     //return min;
 }
 }
-void F_max(int[] array, out int max)
+void F_max(double[] array, out double max)
 {
-    max = arr[0];
+    max = array[0];
     for(int j = 1; j < array.Length; j++)
     {
         if(array[j] > max) max = array[j];
@@ -35,10 +48,12 @@ void F_max(int[] array, out int max)
 }
 
 //----------------------------------------------------------------------------------
-PrintArray(arr);
+double[] array = FillArray(20, -99, 99);
+PrintArray(array);
 //int min = arr[0];
-F_min(arr, out int min);
+F_min(array, out double min);
 System.Console.WriteLine($" Минимальный элемент массива: {min}");
 //int max = 0;
-F_max(arr, out int max);
+F_max(array, out double max);
 System.Console.WriteLine($" Максимальный элемент массива: {max}");
+System.Console.WriteLine($" Разница между максимальным и минимальным элементами массива: {max-min}");
